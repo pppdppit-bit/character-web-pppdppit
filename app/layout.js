7a5055ebcafaaede257793deb725f271c751fa80
pppdppit-bit <pppdppit@gmail.com>
2026-02-26 01:17:24 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 export const metadata = {
   title: "인물관계도 메이커",
   description: "드래그 앤 드롭으로 쉽게 만드는 인물관계도",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({ children }) {
